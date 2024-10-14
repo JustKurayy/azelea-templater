b787a6c33335c305859ac8e03d332e2f6a005be5
dev <302685723@student.rocmondriaan.nl>
2024-10-14 15:58:26 +0200

    if statement support

--- src/Loom.php
+++ src/Loom.php
@@ -2,8 +2,6 @@
 
 namespace Azelea\Templater;
 
-use Locale;
-
 class Loom {
     protected $variables = [];
     private $locales;
@@ -11,9 +9,14 @@ class Loom {
     public function __construct(array $variables) {
         $this->variables = $variables;
         $this->locales = new Locales();
-        $this->locales->chooseLanguage("en");
+        $this->locales->chooseLanguage("en"); //temporary solution
     }
 
+    /**
+     * Returns a HTML page with all the data parsed.
+     * @param string $template
+     * @return HTML Echoes the parsed HTML page
+     */
     public function render(string $template) {
         $dir = substr(dirname(__DIR__), 0, strpos(dirname(__DIR__), "\\vendor\\"));
         $templatePath = $dir . "/src/pages/" . $template;
@@ -35,6 +38,11 @@ class Loom {
         echo ob_get_clean();
     }
 
+    /**
+     * Replaces all @ tags in the Loom template files.
+     * @param string $content
+     * @return string A blob of HTML text
+     */
     public function processDirectives(string $content): string {
         // Replace @foreach
         $content = preg_replace_callback('/@foreach\s*\(\s*\$(\w+)\s+as\s+\$(\w+)\s*\)/', function ($matches) {
@@ -52,25 +60,31 @@ class Loom {
         // Replace @endfor
         $content = str_replace('@endfor', '<?php endfor; ?>', $content);
 
+        // Replace @if
+        $content = preg_replace('/@if\s*\(\s*(.+?)\s*\)/', '<?php if ($1): ?>', $content);
+
+        // Replace @endif
+        $content = str_replace('@endif', '<?php endif; ?>', $content);
+
         // Replace @yield
         $content = preg_replace('/@yield\s*\(\s*([^()]+)\s*\)/', '<?php echo $1; ?>', $content);
 
         // Replace @asset
-        $content = preg_replace('/@asset\(\s*\'([^\"]+)\'\s*\)/', '<?php echo "/assets/$1"; ?>', $content);
+        $content = preg_replace('/@asset\s*\(\s*\'([^\"]+)\'\s*\)/', '<?php echo "/assets/$1"; ?>', $content);
 
         // Replace @lang
-        $content = preg_replace_callback('/@lang\(\s*\'([^\']+)\'\s*\)/', function ($matches) {
+        $content = preg_replace_callback('/@lang\s*\(\s*\'([^\']+)\'\s*\)/', function ($matches) {
             $key = trim($matches[1]);
             $locales = $this->locales->getLocale($key);
             return "<?php echo '$locales'; ?>"; // Remove quotes around $params to pass as variables
         }, $content);
 
         // Replace @class
-        $content = preg_replace_callback('/@class\(\s*\'?(\w+)\'?\s*\)->(\w+)\s*\(\s*(.+?)\s*\)/', function ($matches) {
+        $content = preg_replace_callback('/@class\s*\(\s*\'?(\w+)\'?\s*\)->(\w+)\s*\(\s*(.+?)\s*\)/', function ($matches) {
             $className = trim($matches[1]);
             $method = trim($matches[2]);
             $params = trim($matches[3]);
-            return "<?php echo $$className->$method($params); ?>"; // Remove quotes around $params to pass as variables
+            return "<?php echo \$$className->$method($params); ?>"; // Remove quotes around $params to pass as variables
         }, $content);
 
         return $content;
